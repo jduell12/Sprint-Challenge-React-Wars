eaa66bac0f2aaea0d1a51e1800111df209e99dba
Jessica <duellje@oregonstate.edu>
2020-06-12 11:32:55 -0400

    getting data from API into characterList

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character'
+import {BASE_URL} from './constants/index'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characterList, characterDataChange] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+      .then(res => {
+        debugger
+        characterDataChange(res.data.results);
+      })
+      .catch(err => {
+        debugger
+        console.log(err);
+      })
+
+      return ()=>{}
+  }, []);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* <Character /> */}
     </div>
   );
 }
